f1c7b43559ddcbe4aa988f99f8269033810082d3
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-15 15:06:07 +0300

    Updating Market.cpp

--- Market.cpp
+++ Market.cpp
@@ -1,1 +1,28 @@
 #include "Market.h"
+
+Market& Market::getInstance()
+{
+	static Market instance;
+	return instance;
+}
+
+void Market::printCatalogue() const
+{
+	std::cout << "--------     MARKET CATALOGUE     --------" << std::endl;
+
+	for (const auto& [product, item] : marketCatalogue)
+	{
+		std::cout << toString(product) <<  "   Quantity: " << item.getQuantity() <<  
+			"   Price: " << item.getPrice() << std::endl;
+	}
+}
+
+MarketItem& Market::getItem(PossibleProducts product)
+{
+	return marketCatalogue.at(product);
+}
+
+const MarketItem& Market::getItem(PossibleProducts product) const
+{
+	return marketCatalogue.at(product);
+}
