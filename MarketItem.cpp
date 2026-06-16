98d0625d74174c8a81ca6f790c41edfd8c526d6e
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-16 19:38:11 +0300

    Adding function implementations to MarketItem.cpp

--- MarketItem.cpp
+++ MarketItem.cpp
@@ -1,1 +1,31 @@
 #include "MarketItem.h"
+
+unsigned MarketItem::getQuantity() const
+{
+	return quantity;
+}
+
+void MarketItem::setPrice(double price_)
+{
+	if (price_ < 0)
+	{
+		throw std::invalid_argument("Price cannot be a negative number!");
+	}
+
+	price = price_;
+}
+
+double MarketItem::getPrice() const
+{
+	return price;
+}
+
+void MarketItem::reduceQuantity(unsigned amount)
+{
+	quantity -= amount;
+}
+
+void MarketItem::increaseQuantity(unsigned amount)
+{
+	quantity += amount;
+}
