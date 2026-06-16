06e38e59103d907b14e1e1118f47c27c04d3134f
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-17 00:09:39 +0300

    Uploading UserFactory.h

---
+++ UserFactory.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "User.h"
+#include "Player.h"
+#include "TaskManager.h"
+#include "MarketManager.h"
+
+class UserFactory
+{
+public:
+
+    static std::unique_ptr<User> create(const std::string& role,
+        const std::string& id,
+        const std::string& username,
+        const std::string& password)
+    {
+        if (role == "Player") return std::make_unique<Player>(username, password);
+        if (role == "TaskManager") return std::make_unique <TaskManager>(username, password);
+        if (role == "MarketManager") return std::make_unique<MarketManager>(username, password);
+
+        return nullptr;
+    }
+};
+
