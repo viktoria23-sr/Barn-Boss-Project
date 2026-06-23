af7b1eb5a5013820e89d3dee2a7a54f4cec56011
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 22:45:54 +0300

    Updating User.cpp

--- User.cpp
+++ User.cpp
@@ -1,8 +1,11 @@
 #include "User.h"
 
+User::User(const std::string& id_, const std::string& username_, const std::string& password_) :
+    id(id_), username(username_), password(password_) { }
+
 bool User::isAccuratePassword(const std::string& password_) const
 {
-    if (password_.length() < 3)
+    if (password_.length() < MIN_PASSWORD_LENGTH)
     {
         return false;
     }
@@ -10,8 +13,19 @@ bool User::isAccuratePassword(const std::string& password_) const
     return true;
 }
 
+void User::logout()
+{
+    std::println("User '{}' is logging out...", username);
+}
+
 void User::changePassword(const std::string& oldPassword, const std::string& newPassword)
 {
+    if (password != oldPassword)
+    {
+        std::println("Error: Incorrect old password!");
+        return;
+    }
+
     if (isAccuratePassword(newPassword))
     {
         if (oldPassword == newPassword)
@@ -19,7 +33,7 @@ void User::changePassword(const std::string& oldPassword, const std::string& new
             std::println("The new password is the same as the old. Choose another one!");
             return;
         }
-    
+
         password = newPassword;
     }
 }
@@ -37,4 +51,14 @@ const std::string& User::getId() const
 const std::string& User::getUsername() const
 {
     return username;
+}
+
+const std::string& User::getPassword() const
+{
+    return password;
+}
+
+void User::save(std::ostream& out) const
+{
+    out << id << " " << username << " " << password;
 }
