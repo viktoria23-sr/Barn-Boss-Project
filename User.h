79abf7e394960c8e90cea39fb8a123e09f5386cd
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 22:41:18 +0300

    Updating User.h

--- User.h
+++ User.h
@@ -10,12 +10,20 @@ protected:
 	std::string password;
 
 public:
+	User(const std::string& id_, const std::string& username_, const std::string& password_);
+	
 	bool isAccuratePassword(const std::string& password) const;
 	void logout();
 	void changePassword(const std::string& oldPassword, const std::string& newPassword);
-	virtual void profileInfo() const; //=0 ???
+	virtual void profileInfo() const = 0;
+	virtual ~User() = default;
 
-	std::string getId() const;
-	std::string getUsername() const;
+	const std::string& getId() const;
+	const std::string& getUsername() const;
+	const std::string& getPassword() const;
+
+	virtual void save(std::ostream& out) const;
+	virtual void load(std::istream& in) {}
+	virtual std::string getRole() const = 0;
 };
 
