89d00adf2765bb5c2ad4f8a12547aa4f17064e4a
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 21:38:22 +0300

    Updating Market.cpp

--- Market.cpp
+++ Market.cpp
@@ -1,5 +1,17 @@
 #include "Market.h"
 
+Market::Market()
+{
+	marketCatalogue.insert({ PossibleProducts::WheatSeed, MarketItem(20,10) });
+	marketCatalogue.insert({ PossibleProducts::CornSeed, MarketItem(20,15) });
+	marketCatalogue.insert({ PossibleProducts::Chicken, MarketItem(10,25) });
+	marketCatalogue.insert({ PossibleProducts::Cow, MarketItem(5,50) });
+	marketCatalogue.insert({ PossibleProducts::Wheat, MarketItem(20,15) });
+	marketCatalogue.insert({ PossibleProducts::Corn, MarketItem(20,20) });
+	marketCatalogue.insert({ PossibleProducts::Egg, MarketItem(15,30) });
+	marketCatalogue.insert({ PossibleProducts::Milk, MarketItem(10,60) });
+}
+
 Market& Market::getInstance()
 {
 	static Market instance;
