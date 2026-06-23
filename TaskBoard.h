59b9d13350ed3aac677fda8b7410333adb44371d
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 22:31:32 +0300

    Updating TaskBoard.h

--- TaskBoard.h
+++ TaskBoard.h
@@ -1,13 +1,22 @@
 #pragma once
-#include <vector>
+#include <map>
 #include "Task.h"
+#include <print>
 
 class TaskBoard
 {
 protected:
 	std::vector<Task> tasks;
-	static int uncompletedTasks;
+	TaskBoard();
 
 public:
-	TaskBoard();
+	const std::map<size_t, Task>& getTasks() const;
+
+	TaskBoard(const TaskBoard& other) = delete;
+	TaskBoard& operator=(const TaskBoard& other) = delete;
+
+	static TaskBoard& getInstance();
+	void addTask(const Task& newTask);
+	void removeTaskById(size_t taskId);
+	Task* findTaskById(size_t taskId);
 };
