8f483a7dc3aa5e9a0958586f84c595627560c752
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 21:53:26 +0300

    Updating Plant.cpp

--- Plant.cpp
+++ Plant.cpp
@@ -1,5 +1,23 @@
 #include "Plant.h"
 
+Plant::Plant(PossibleProducts type_) : type(type_)
+{
+	if (type != PossibleProducts::WheatSeed && type != PossibleProducts::CornSeed)
+	{
+		throw std::invalid_argument("A plant in a farm must be a wheat seed or a corn seed!");
+	}
+
+	if (type == PossibleProducts::WheatSeed)
+	{
+		requiredCycles = 3;
+	}
+
+	if (type == PossibleProducts::CornSeed)
+	{
+		requiredCycles = 4;
+	}
+}
+
 void Plant::increaseCurrentCycle()
 {
 	currentCycle++;
