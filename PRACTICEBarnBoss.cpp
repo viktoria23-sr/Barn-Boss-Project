423b394a971adf9b85556a146eed46b5c43c26a1
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 18:45:32 +0300

    Adding main function

---
+++ PRACTICEBarnBoss.cpp
@@ -0,0 +1,14 @@
+﻿// PRACTICEBarnBoss.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Game.h"
+
+int main()
+{
+    Game barnBoss;
+
+    barnBoss.start();
+
+    return 0;
+}
