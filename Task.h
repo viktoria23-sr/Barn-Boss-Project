5f94acbc9aa94651be0f5d6a53ad32543e9d732c
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 22:29:05 +0300

    Updating Task.h

--- Task.h
+++ Task.h
@@ -5,17 +5,23 @@
 class Task
 {
 protected:
-	std::string id;
+	size_t id;
 	PossibleProducts requiredProduct;
-	unsigned long requiredQuantity;
+	unsigned requiredQuantity;
 	size_t rewardBalance;
 	size_t rewardScore;
 	bool isCompleted = false;
 
 public:
 	Task() = delete;
-	Task(const std::string& id_, PossibleProducts requiredProduct_, unsigned long requiredQuantity_, size_t rewardBalance_,
-		size_t rewardScore_) : id(id_), requiredProduct(requiredProduct_), requiredQuantity(requiredQuantity_),
-		rewardBalance(rewardBalance_), rewardScore(rewardScore_) {}
+	Task(size_t id_, PossibleProducts requiredProduct_, unsigned requiredQuantity_, size_t rewardBalance_,
+		size_t rewardScore_);
+	PossibleProducts getRequiredProduct() const;
+	unsigned getRequiredQuantity() const;
+	size_t getRewardBalance() const;
+	size_t getRewardScore() const;
+	size_t getId() const;
+	bool getIsCompleted() const;
+	void setCompleted();
 };
 
