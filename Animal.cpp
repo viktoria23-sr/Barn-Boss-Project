400df77cb0f3e4074df7b35ee8c5eda6a6c79e9a
viktoria23-sr <viktoriasr@uni-sofia.bg>
2026-06-23 20:59:23 +0300

    Updating Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -1,6 +1,25 @@
 #include "Animal.h"
 #include "Plant.h"
 
+Animal::Animal(PossibleProducts type_) : type(type_)
+{
+	if (type != PossibleProducts::Cow && type != PossibleProducts::Chicken)
+	{
+		throw std::invalid_argument("An animal should be a chicken or a cow!");
+	}
+
+	if (type == PossibleProducts::Chicken)
+	{
+		requiredCycles = 3;
+	}
+
+	if (type == PossibleProducts::Cow)
+	{
+		requiredCycles = 5;
+	}
+}
+
+
 void Animal::increaseCurrentCycle()
 {
 	currentCycle++;
